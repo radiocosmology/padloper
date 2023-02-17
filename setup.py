16e8cb7cd937368eaf7d969809d90d0d1ee01e98
iuliano <jeff.iuliano@gmail.com>
2023-02-17 16:37:52 +0000

    basics to make python module installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='padloper',
+      version='0.1',
+      description='',
+      url='https://github.com/radiocosmology/padloper',
+      author='',
+      author_email='',
+      license='',
+      packages=['padloper'],
+      zip_safe=False)
