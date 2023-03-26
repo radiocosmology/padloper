1fc14f72fd65a0dc92b3d1a5fa789201c3284986
Adam Hincks <ahincks@secchi.astro.utoronto.ca>
2023-03-25 21:47:35 -0400

    Intermediate commit before removing structure.py.

--- padloper/__init__.py
+++ padloper/__init__.py
@@ -1,4 +1,11 @@
 # For relative imports to work in Python 3.6
 # https://stackoverflow.com/a/49375740
+CONTINUE HERE: get all the importing working properly.
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from structure import *
+from base import *
+from edges import *
+from exceptions import *
+from permissions import *
+from component_nodes import *
+from property_nodes import *
+from flag_nodes import *
